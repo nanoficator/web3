3f1c184345c0a0f4574f9bc7a143fdd7a8736745
Admin <mkuzivanov@gmail.com>
2019-11-24 22:16:55 +0300

    Web 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,24 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import service.BankClientService;
 import model.BankClient;
 import servlet.ApiServlet;
+import servlet.MoneyTransactionServlet;
+import servlet.RegistrationServlet;
+import servlet.ResultServlet;
+
+import javax.sql.rowset.spi.TransactionalWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         ApiServlet apiServlet = new ApiServlet();
+        ResultServlet resultServlet = new ResultServlet();
+        MoneyTransactionServlet moneyTransactionServlet = new MoneyTransactionServlet();
+        RegistrationServlet registrationServlet = new RegistrationServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(apiServlet), "/api/*");
+        context.addServlet(new ServletHolder(resultServlet), "/result");
+        context.addServlet(new ServletHolder(moneyTransactionServlet), "/transaction");
+        context.addServlet(new ServletHolder(registrationServlet), "/registration");
 
         Server server = new Server(8080);
         server.setHandler(context);
